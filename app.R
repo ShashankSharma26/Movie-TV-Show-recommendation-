b8d25f0964055e20da27ec0dd0784583837a9068
Shashank Sharma <ssha0028@gmail.com>
2020-05-03 23:52:35 +1000

    Shiny code update

--- app.R
+++ app.R
@@ -1,33 +1,50 @@
 library(shiny)
 library(shinyWidgets)
+library(shinydashboard)
+library(httr)
+library(jsonlite)
+library(jpeg)
+library(purrr)
 
 ######################
 ##LOADING THE FILES###
 ######################
-title_info = read.csv("/Users/shashanksharma/github/Movie-TV-Show-recommendation-/Data preperation/title_information.csv")
-cast_info = read.csv("/Users/shashanksharma/github/Movie-TV-Show-recommendation-/Data preperation/cast_information.csv")
+title_info = read.csv("Data preperation/title_information.csv")
+cast_info = read.csv("Data preperation/cast_information.csv")
 
-title_info$actual_value = title_info$averageRating * title_info$numVotes
+colnames(title_info) = c("X" ,"titleId",'Title', "region" ,'Language',"Type", "isAdult",'Year','Genre','Rating',"numVotes")
+
+
+
+title_info$multiplier = as.numeric(as.character(cut(title_info$numVotes, breaks=c(0,100, 200,300,500,750,1000,2500,5000,10000,25000,50000, 2224462), 
+                                                    labels=c( 0.50, 0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90, .95, 0.97, 1))))
+
+title_info$truerating = with(title_info,multiplier * Rating)
 
 #################################
 ###USER INTERFACE OF THE APP#####
 #################################
 ui <- fluidPage(
+ 
 
-tags$style("body {background-color: #A9A9A9; }"),
+#<img src="https://image.tmdb.org/t/p/w342/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg" width=450 height=450></img>,
+#tags$img(src = "https://image.tmdb.org/t/p/w342/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg"),
 
-headerPanel(h2(strong("Create your own customised Watchlist "), 
-               div(img(src="main_screen.jpg", height = 500, width = 1000,align="center")),
+titlePanel(h2(strong("Create Your Own Customised Watchlist"), 
+               div(img(src="main.jpg", height = "500px", width = "900px" ,align="center")),
+              
                align = "center", 
-               style = "color:black; font-family: 'baskerville';",
-               color = "ghostwhite")), ##Main heading
+               style = "color:crimson; font-family: 'baskerville';",
+              setBackgroundColor("#f0f0f5"),
+               color = "ghostwhite"),
+            windowTitle = "Movie recommender by Shashank"), ##Main heading
 
 
 sidebarLayout(
   
   sidebarPanel(tags$style(".well {background-color: #DCDCDC; }"),
                   radioButtons("type", #to select 
-                                     h4(strong("I want to watch:"),style = "color:crimson;font-family: 'baskerville';"),
+                                     h4(strong("Select type:"),style = "color:crimson;font-family: 'baskerville';"),
                                      choices = c("Movie", "Series","Anything"),
                                      selected = c("Movie"),
                                      inline = TRUE),
@@ -40,9 +57,9 @@ sidebarLayout(
                   
                   
                   radioButtons("add_genre", #to select 
-                                     h4(strong("Genre Selection"),style = "color:crimson;font-family: 'baskerville';" ),
-                                     choices = c("Surprise me", "I want to select the genres"),
-                                     selected = c("Surprise me")
+                                     h4(strong("Select Genre"),style = "color:crimson;font-family: 'baskerville';" ),
+                                     choices = c("Any genre", "I want to select the genres"),
+                                     selected = c("Any genre")
                                      ),
                   
                   conditionalPanel(condition = "input.add_genre == 'I want to select the genres'",
@@ -66,7 +83,7 @@ sidebarLayout(
                               sep = ""),
                   
                   sliderInput("rating", 
-                              h4(strong("Select the IMDB rating range"),style = "color:crimson;font-family: 'baskerville';"),
+                              h4(strong("Select IMDB rating range"),style = "color:crimson;font-family: 'baskerville';"),
                               0,10, 
                               value = c(0, 10),
                               sep = ""),
@@ -84,7 +101,9 @@ sidebarLayout(
                                  multiple = TRUE)
               
                    ),
-         mainPanel(DT::dataTableOutput("table"),
+         mainPanel(h2("Search Results", align = "center", style = "color:black;font-family: 'baskerville';"),
+                  
+                   DT::dataTableOutput("table"),
                    imageOutput("image"))
 )
 
@@ -96,7 +115,7 @@ sidebarLayout(
 ##################
 server <-  function(input,output,session){
 
-image = 0
+#image = 0
   
 #Updating the actors selectize inputs with values
 updateSelectizeInput(session, 
@@ -112,36 +131,36 @@ updateSelectizeInput(session,
 # Filter data based on selections
 output$table <- DT::renderDataTable(DT::datatable({
     data <- title_info
-    
+
 # Handling type radiobutton for movie/tv show
 if (input$type == 'Movie'){
-      data = data[which(data$titleType == 'movie'),]
+      data = data[which(data$Type == 'movie'),]
 }
     
 else if (input$type == 'Series'){
-  data = data[-which(data$titleType == 'movie'),]
+  data = data[-which(data$Type == 'movie'),]
 }
     
 #Handling langauge filters
 if (input$lang == 'English'){
-    data = data[which(data$language == 'English'),]
+    data = data[which(data$Language == 'English'),]
 }
 
 
 else if((input$lang == 'Hindi')){
-  data = data[which(data$language == 'Hindi'),]
+  data = data[which(data$Language == 'Hindi'),]
 }
 
 #Handling Genres
 if(input$add_genre == "I want to select the genres"){
- data =  data[grep(paste(input$select_genre, collapse="|"), data$genres),]
+ data =  data[grep(paste(input$select_genre, collapse="|"), data$Genre),]
 }
     
 #Handlin year range
-data = data[which(data$startYear >= input$year[1] & data$startYear <= input$year[2]),]    
+data = data[which(data$Year >= input$year[1] & data$Year <= input$year[2]),]    
 
 #Handling ratings range
-data = data[which(data$averageRating >= input$rating[1] & data$averageRating <= input$rating[2]),]  
+data = data[which(data$Rating >= input$rating[1] & data$Rating <= input$rating[2]),]  
 
 #handling actors names
 actor_list = unique(cast_info[grep(paste(input$actors, collapse="|"), cast_info$name),]$tconst)
@@ -152,31 +171,39 @@ director_list = unique(cast_info[grep(paste(input$director, collapse="|"), cast_
 data = data[which(data$titleId %in% director_list ),]
 
 
-##Managing the output  
-
-
-data = data[which(data$numVotes >= mean(data$numVotes)),]
-
+# ##Managing the output  
+# selectedrowindex <<-     input$table_rows_current[length(input$table_rows_current)]
+# selectedrowindex <<- as.numeric(selectedrowindex)
+# 
+# 
+# data[selectedrowindex,]$Posters = map(data[selectedrowindex,]$titleId,poster)
 
 
 if ((input$type == 'Anything') && (input$lang == 'Both')){
-  data[with(data,order(-actual_value)), c('title','titleType','language','genres','averageRating','startYear')]
+  
+  
+  data[with(data,order(-truerating,-Rating )), c('Title','Type','Language','Genre','Rating','Year')] 
   
 }
 
 else if(input$type == 'Anything'){
-  data[with(data,order(-actual_value)), c('title','titleType','genres','averageRating','startYear')]
+
+  data[with(data,order(-truerating,-Rating )),  c('Title','Type','Genre','Rating','Year')] 
+
 }
 
 else if(input$lang == 'Both'){
-  data[with(data,order(-actual_value)), c('title','language','genres','averageRating','startYear')]
+
+  data[with(data,order(-truerating,-Rating)),  c('Title','Language','Genre','Rating','Year')]
 }
 
 else{
-  data[with(data,order(-actual_value)), c('title','genres','averageRating','startYear')]
+
+  data[with(data,order(-truerating,-Rating)),  c('Title','Genre','Rating','Year')]
 }
-  }, options = 
-  list(searching = FALSE,paging = TRUE,
+  }, rownames = FALSE, 
+  options = 
+  list(searching = FALSE,paging = TRUE, pageLength = 15,
        language = list(
          zeroRecords = '<img src="error.jpg" width=450 height=450></img>'))              
   ))
